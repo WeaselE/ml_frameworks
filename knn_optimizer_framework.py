16bef4af27b4b132c112ac4863b79af88a3d2854
weaustin <weaustin@iu.edu>
2022-09-11 18:46:46 -0400

    formatting with black formatter

--- knn_optimizer_framework.py
+++ knn_optimizer_framework.py
@@ -5,7 +5,7 @@ from itertools import combinations
 import pickle
 
 
-class knn_framework():
+class knn_framework:
     def __init__(self, X: pd.DataFrame, y: pd.DataFrame) -> None:
         self.X = X
         self.y = y
@@ -15,23 +15,43 @@ class knn_framework():
 
     def __repr__(self) -> str:
         if self.optimal_features is not None and self.optimal_n is not None:
-            s = str(self.X.head) + '\n\n' + str(self.y.head()) + '\n\n' + \
-                str(self.optimal_features) + '\n\n' + str(self.optimal_n)
+            s = (
+                str(self.X.head)
+                + "\n\n"
+                + str(self.y.head())
+                + "\n\n"
+                + str(self.optimal_features)
+                + "\n\n"
+                + str(self.optimal_n)
+            )
             return s
         elif self.optimal_features is not None:
-            s = s = str(self.X.head) + '\n\n' + str(self.y.head()
-                                                    ) + '\n\n' + str(self.optimal_features)
+            s = s = (
+                str(self.X.head)
+                + "\n\n"
+                + str(self.y.head())
+                + "\n\n"
+                + str(self.optimal_features)
+            )
             return s
         elif self.optimal_n is not None:
-            s = str(self.X.head) + '\n\n' + str(self.y.head()) + \
-                '\n\n' + str(self.optimal_n)
+            s = (
+                str(self.X.head)
+                + "\n\n"
+                + str(self.y.head())
+                + "\n\n"
+                + str(self.optimal_n)
+            )
             return s
-        s = str(self.X.head()) + '\n\n' + str(self.y.head())
+        s = str(self.X.head()) + "\n\n" + str(self.y.head())
         return s
 
-    def data_split(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    def data_split(
+        self,
+    ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         X_train, X_test, y_train, y_test = train_test_split(
-            self.X, self.y, test_size=0.5, random_state=0)
+            self.X, self.y, test_size=0.5, random_state=0
+        )
         return (X_train, X_test, y_train, y_test)
 
     def feature_optimizer(self) -> list:
@@ -39,19 +59,24 @@ class knn_framework():
         X_train, X_test, y_train, y_test = self.data_split()
         feature_size = len(X_train.columns)
         for step in range(1, feature_size - 1):
-            print(f'combining features with feature size of: {step}')
+            print(f"combining features with feature size of: {step}")
             for combination in list(combinations(range(0, feature_size), r=step)):
                 combination = list(combination)
                 if combination:
                     X_train_temp = X_train.iloc[:, combination]
                     X_test_temp = X_test.iloc[:, combination]
                     model = KNeighborsClassifier().fit(X_train_temp, y_train)
-                    result = (step, combination, model.score(X_test_temp, y_test), [
-                              X_train.columns[i] for i in combination])
+                    result = (
+                        step,
+                        combination,
+                        model.score(X_test_temp, y_test),
+                        [X_train.columns[i] for i in combination],
+                    )
                     results.append(result)
         best_result = max(results, key=lambda x: x[2])
         print(
-            f"\noptimal feature combination: {best_result[3]}\n score: {round(best_result[2]*100,2)}\n")
+            f"\noptimal feature combination: {best_result[3]}\n score: {round(best_result[2]*100,2)}\n"
+        )
         self.optimal_features = best_result
 
         # print(f'\n{sorted(results, key=lambda x : (x[2], x[0]), reverse=False)}')
@@ -61,43 +86,38 @@ class knn_framework():
         results = list()
         X_train, X_test, y_train, y_test = self.data_split()
         if self.optimal_features is None:
-            print('Optimal features not found, using all features\n')
+            print("Optimal features not found, using all features\n")
             n_max = int(len(X_train) / 2)
             for n in range(1, n_max):
-                model = KNeighborsClassifier(
-                    n_neighbors=n).fit(X_train, y_train)
+                model = KNeighborsClassifier(n_neighbors=n).fit(X_train, y_train)
                 result = (n, model.score(X_test, y_test))
                 results.append(result)
         else:
             X_train = X_train.iloc[:, self.optimal_features[1]]
             X_test = X_test.iloc[:, self.optimal_features[1]]
-            print(
-                f'Optimizing n for optimal features: {self.optimal_features[3]}\n')
+            print(f"Optimizing n for optimal features: {self.optimal_features[3]}\n")
             n_max = int(len(X_train) / 2)
             for n in range(1, n_max):
-                model = KNeighborsClassifier(
-                    n_neighbors=n).fit(X_train, y_train)
+                model = KNeighborsClassifier(n_neighbors=n).fit(X_train, y_train)
                 result = (n, model.score(X_test, y_test))
                 results.append(result)
         best_result = max(results, key=lambda x: x[1])
-        print(
-            f'\noptimal n: {best_result[0]}\nscore: {round(best_result[1]*100,2)}\n')
+        print(f"\noptimal n: {best_result[0]}\nscore: {round(best_result[1]*100,2)}\n")
         self.optimal_n = best_result
         return results
 
     def final_score(self):
         if self.optimal_features is None:
-            print('Optimal features have not yet been found for this dataset.\n')
+            print("Optimal features have not yet been found for this dataset.\n")
 
         elif self.optimal_n is None:
-            print('Optimal n-neighbors n has not yet been found for this dataset.\n')
+            print("Optimal n-neighbors n has not yet been found for this dataset.\n")
         else:
             X = self.X.iloc[:, self.optimal_features[1]]
             y = self.y
-            model = KNeighborsClassifier(
-                n_neighbors=self.optimal_n[0]).fit(X, y)
+            model = KNeighborsClassifier(n_neighbors=self.optimal_n[0]).fit(X, y)
             result = model.score(X, y)
-            print(f'final score: {round(result * 100, 2)}\n')
+            print(f"final score: {round(result * 100, 2)}\n")
             return result
 
     def save_model(self):
@@ -106,18 +126,18 @@ class knn_framework():
         if self.optimal_features is not None:
             X = X.iloc[:, self.optimal_features[1]]
         if self.optimal_n is not None:
-            model = KNeighborsClassifier(
-                n_neighbors=self.optimal_n[0]).fit(X, y)
+            model = KNeighborsClassifier(n_neighbors=self.optimal_n[0]).fit(X, y)
         else:
             model = KNeighborsClassifier().fit(X, y)
-        with open('knn_model.pkl', 'wb') as f:
+        with open("knn_model.pkl", "wb") as f:
             pickle.dump(model, f)
-        print('model saved to knn_model.pkl')
+        print("model saved to knn_model.pkl")
         return model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sklearn.datasets import load_wine
+
     data = load_wine(as_frame=True)
     data_X = data.data
     data_y = data.target
--- knn_optimizer_framework_revised.py
+++ knn_optimizer_framework_revised.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.impute import SimpleImputer
 
 
-class knn_framework():
+class knn_framework:
     def __init__(self, X: pd.DataFrame, y: pd.DataFrame) -> None:
         self.X = X
         self.y = y
@@ -22,18 +22,35 @@ class knn_framework():
 
     def __repr__(self) -> str:
         if self.optimal_features is not None and self.optimal_n is not None:
-            s = str(self.X.head) + '\n\n' + str(self.y.head()) + '\n\n' + \
-                str(self.optimal_features) + '\n\n' + str(self.optimal_n)
+            s = (
+                str(self.X.head)
+                + "\n\n"
+                + str(self.y.head())
+                + "\n\n"
+                + str(self.optimal_features)
+                + "\n\n"
+                + str(self.optimal_n)
+            )
             return s
         elif self.optimal_features is not None:
-            s = s = str(self.X.head) + '\n\n' + str(self.y.head()
-                                                    ) + '\n\n' + str(self.optimal_features)
+            s = s = (
+                str(self.X.head)
+                + "\n\n"
+                + str(self.y.head())
+                + "\n\n"
+                + str(self.optimal_features)
+            )
             return s
         elif self.optimal_n is not None:
-            s = str(self.X.head) + '\n\n' + str(self.y.head()) + \
-                '\n\n' + str(self.optimal_n)
+            s = (
+                str(self.X.head)
+                + "\n\n"
+                + str(self.y.head())
+                + "\n\n"
+                + str(self.optimal_n)
+            )
             return s
-        s = str(self.X.head()) + '\n\n' + str(self.y.head())
+        s = str(self.X.head()) + "\n\n" + str(self.y.head())
         return s
 
     def feature_optimizer(self) -> list:
@@ -43,7 +60,7 @@ class knn_framework():
         feature_size = len(X.columns)
 
         for step in range(1, feature_size - 1):
-            print(f'combining features with feature size of: {step}')
+            print(f"combining features with feature size of: {step}")
             for combination in list(combinations(range(0, feature_size), r=step)):
                 combination = list(combination)
                 if combination:
@@ -57,7 +74,8 @@ class knn_framework():
         optimal_result = max(results, key=lambda x: x[1])
 
         print(
-            f"\noptimal feature combination: {optimal_result[2]}\n score: {round(optimal_result[1]*100,2)}\n")
+            f"\noptimal feature combination: {optimal_result[2]}\n score: {round(optimal_result[1]*100,2)}\n"
+        )
 
         self.optimal_features_combination = optimal_result[0]
         self.optimal_feature_score = optimal_result[1]
@@ -71,22 +89,19 @@ class knn_framework():
         y = self.y
 
         if self.optimal_features_combination is None:
-            print('Optimal features not found, using all features\n')
+            print("Optimal features not found, using all features\n")
             n_max = int(len(X) / 2)
             for n in range(1, n_max):
-                model = KNeighborsClassifier(
-                    n_neighbors=n).fit(X, y)
+                model = KNeighborsClassifier(n_neighbors=n).fit(X, y)
                 score = mean(cross_val_score(model, X, y))
                 result = (n, score)
                 results.append(result)
         else:
             X = X.iloc[:, self.optimal_features_combination]
-            print(
-                f'Optimizing n for optimal features: {self.optimal_features_names}\n')
+            print(f"Optimizing n for optimal features: {self.optimal_features_names}\n")
             n_max = int(len(X) / 2)
             for n in range(1, n_max):
-                model = KNeighborsClassifier(
-                    n_neighbors=n).fit(X, y)
+                model = KNeighborsClassifier(n_neighbors=n).fit(X, y)
                 score = mean(cross_val_score(model, X, y))
                 result = (n, score)
                 results.append(result)
@@ -94,43 +109,40 @@ class knn_framework():
         optimal_result = max(results, key=lambda x: x[1])
         optimal_score = optimal_result[1]
         optimal_n = optimal_result[0]
-        print(
-            f'\noptimal n: {optimal_n}\nscore: {round(optimal_score*100,2)}\n')
+        print(f"\noptimal n: {optimal_n}\nscore: {round(optimal_score*100,2)}\n")
         self.optimal_n = optimal_n
         self.optimal_n_score = optimal_score
         return results
 
     def final_score(self) -> list:
         if self.optimal_features_combination is None:
-            print('Optimal features have not yet been found for this dataset.\n')
+            print("Optimal features have not yet been found for this dataset.\n")
             return []
         elif self.optimal_n is None:
-            print('Optimal n-neighbors n has not yet been found for this dataset.\n')
+            print("Optimal n-neighbors n has not yet been found for this dataset.\n")
             return []
         else:
             X = self.X.iloc[:, self.optimal_features_combination]
             y = self.y
-            model = KNeighborsClassifier(
-                n_neighbors=self.optimal_n).fit(X, y)
+            model = KNeighborsClassifier(n_neighbors=self.optimal_n).fit(X, y)
             result = mean(cross_val_score(model, X, y))
-            print(f'final score: {round(result * 100, 2)}\n')
+            print(f"final score: {round(result * 100, 2)}\n")
             return result
 
-    def save_model(self, file_name: str = 'knn_model') -> KNeighborsClassifier:
+    def save_model(self, file_name: str = "knn_model") -> KNeighborsClassifier:
         X = self.X
         y = self.y
-        file_name += '.pkl'
+        file_name += ".pkl"
         if self.optimal_features_combination is not None:
             X = X.iloc[:, self.optimal_features_combination]
         if self.optimal_n is not None:
-            model = KNeighborsClassifier(
-                n_neighbors=self.optimal_n).fit(X, y)
+            model = KNeighborsClassifier(n_neighbors=self.optimal_n).fit(X, y)
         else:
             model = KNeighborsClassifier().fit(X, y)
 
-        with open(file_name, 'wb') as f:
+        with open(file_name, "wb") as f:
             pickle.dump(model, f)
-        print(f'model saved to {file_name}')
+        print(f"model saved to {file_name}")
 
         return model
 
@@ -142,8 +154,9 @@ class knn_framework():
         y = imputer.transform(y)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sklearn.datasets import load_digits
+
     data = load_digits(as_frame=True)
     data_X = data.data
     data_y = data.target
@@ -151,4 +164,4 @@ if __name__ == '__main__':
     framework.feature_optimizer()
     framework.n_optimizer()
     framework.final_score()
-    framework.save_model('wine_model.pkl')
+    framework.save_model("wine_model.pkl")
